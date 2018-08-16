dd3fde71cb0fae16091c6851ddfc18082ae68073
Pavel <paulk@ikantam.com>
2018-08-16 11:56:24 +0300

    fix: updated reset passwords links into notifications

--- app/Notifications/CreatePassword.php
+++ app/Notifications/CreatePassword.php
@@ -46,7 +46,7 @@ class CreatePassword extends Notification
     {
         return (new MailMessage)
             ->line('New account was successfully created. Please create password.')
-            ->action('Create Password', env('DASHBOARD_URL') . $this->token)
+            ->action('Create Password', url("/passwords/reset/?{$this->token}"))
             ->line('If you did not request a account create, no further action is required.');
     }
 }
--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -46,7 +46,7 @@ class ResetPassword extends Notification
     {
         return (new MailMessage)
             ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password', env('DASHBOARD_URL') . $this->token)
+            ->action('Reset Password', url("/passwords/reset/?{$this->token}"))
             ->line('If you did not request a password reset, no further action is required.');
     }
 }
